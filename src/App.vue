2d5e6dcc816d896f8c8cb73fb77f4f03f3f7e4f8
Michal Zbranek <michal.zbranek.1@gmail.com>
2021-01-30 11:34:49 +0100

    Boxing

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row header" style="border-bottom: 1px solid white;">
+    <h1>Vítejte na mých stránkách</h1>
+    <p class="lead">Jsem Michal Zbranek, programátor ve firmě Tieto</p>
+  </div>
+  <div class="row content">
+    <div class="row h-100 m-0 p-0">
+      <div class="col-9"></div>
+      <div class="col-3" style="border-left: 1px solid white;"><Navbar/></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-left: 15%;
+  padding-top: 3%;
+}
+
+html, body {
+  height: 100%;
+  margin: 0;
+  font-size: 1.2rem;
+}
+
+.box {
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+}
+
+.box .row.header {
+  flex: 0 1 auto;
+  margin-right: 0;
+}
+
+.box .row.content {
+  display: flex;
+  flex-flow: column;
+  flex: 1 1 auto;
+  overflow-y: auto;
+  margin-right: 0;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="nav">
+        <a class="link" href="#">Domů<div class="bla"></div></a>
+        <a class="link" href="#">Projekty<div class="bla"></div></a>
+        <a class="link" href="#">Galerie<div class="bla"></div></a>
+        <a class="link" href="#">O mně<div class="bla"></div></a>
+        <a class="link" href="#">Kontakt<div class="bla"></div></a>
+    </nav>
+</template>
+
+<style>
+.link {
+    color: #ffffff;
+    text-decoration: none;
+    padding: 10px 10px;
+    margin: 0 10px;
+  	position: relative;
+}
+
+.bla {
+	width: 0px;
+    height: 100%;
+    transition: 0.4s;
+    margin-top: -45px;
+	position: absolute;
+    background-color:white;
+    z-index: -1;
+    margin-left: -10px;
+}
+
+.link:hover .bla {
+    margin-top: -45px;
+    width: 100%;
+}
+
+.link:hover {
+    color: #212529;
+}
+</style>
