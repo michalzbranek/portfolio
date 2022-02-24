632b15aef0a5b93c0fe5b17dcd8ca5532ff6bc5f
Michal Zbranek <michal.zbranek.1@gmail.com>
2022-02-24 21:20:47 +0100

    Carousel Caption

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,18 +1,22 @@
 <template>
   <h1 id="portfolio" class="fw-bold mb-5" style="">projekty</h1>
-  <div id="carouselExampleDark" data-bs-interval="false" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div id="carouselExampleDark" data-bs-interval="false" class="carousel carousel-dark slide carousel-fade" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
         <a href="https://michalzbranekblog.herokuapp.com/" target="_blank">
-          <img src="../assets/copy1.png" class="image">
+          <img src="../assets/copy1.png" class="d-block w-100">
         </a>
-        <div class="text">Tento blog jsem si vytvořil pro mé psaní. Ukazuje použití Heroku platformy, Javascript, Node.js a používá MongoDB databázi.</div>
+      <div class="carousel-caption d-none d-md-block">
+        <h4>Tento blog jsem si vytvořil pro mé psaní. Ukazuje použití Heroku platformy, Javascript, Node.js a používá MongoDB databázi.</h4>
+      </div>
       </div>
       <div class="carousel-item">
         <a href="https://michalzbranek.github.io/fcmalenovice/" target="_blank">
-          <img src="../assets/copy3.png" class="image">
+          <img src="../assets/copy3.png" class="d-block w-100">
         </a>
-        <div class="text">Tento web jsem vytvořil pro fotbalový klub, ve kterém jsem dříve hrával. Web používá pouze HTML a CSS a slouží tak pro zachycení základních znalostí vývoje webu.</div>
+      <div class="carousel-caption d-none d-md-block">
+        <h4>Tento web jsem vytvořil pro fotbalový klub, ve kterém jsem dříve hrával. Web používá pouze HTML a CSS a slouží tak pro zachycení základních znalostí vývoje webu.</h4>
+      </div>
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
@@ -33,12 +37,7 @@ h1 {
   font-size: 8vw;
 }
 
-.text {
-  margin-top: 3%;
-  width: 85%;
-}
-
-.image {
-  width: 85%;
+h4 {
+  background-color: white;
 }
 </style>
